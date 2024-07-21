9274163f2080d67261191a65817c804ca6251c1f
SangHuPark <tkdgn5408407@gmail.com>
2024-07-22 02:46:38 +0900

    feat: initial springboot project

---
+++ spring-dekku/src/main/java/dekku/spring_dekku/SpringDekkuApplication.java
@@ -0,0 +1,13 @@
+package dekku.spring_dekku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDekkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDekkuApplication.class, args);
+	}
+
+}
---
+++ spring-dekku/src/test/java/dekku/spring_dekku/SpringDekkuApplicationTests.java
@@ -0,0 +1,13 @@
+package dekku.spring_dekku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDekkuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
