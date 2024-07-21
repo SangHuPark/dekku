d70809c5fd847c9bbff829224b6b387443298383
SangHuPark <tkdgn5408407@gmail.com>
2024-07-22 03:17:52 +0900

    feat: add Swagger

--- spring-dekku/src/main/java/dekku/spring_dekku/global/config/swagger/SwaggerConfig.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/global/config/swagger/SwaggerConfig.java
@@ -1,71 +1,69 @@
 package dekku.spring_dekku.global.config.swagger;
 
-import java.util.Arrays;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
 
-	@Bean
-	public OpenAPI openApi() {
+    @Bean
+    public OpenAPI openApi() {
 
-		Server localServer = new Server();
-		localServer.setDescription("local");
-		localServer.setUrl("http://localhost");
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost");
 
 
-		return new OpenAPI()
-			.info(getInfo())
-			.servers(Arrays.asList(localServer));
+        return new OpenAPI()
+                .info(getInfo())
+                .servers(Arrays.asList(localServer));
 
-	}
+    }
 
-	private Info getInfo() {
-		return new Info()
-			.title("SSAFY TRIP API")
-			.description("SSAFY TRIP DOCS");
-	}
+    private Info getInfo() {
+        return new Info()
+                .title("SSAFY TRIP API")
+                .description("SSAFY TRIP DOCS");
+    }
 
-	// @Bean
-	// public GroupedOpenApi securityGroup() {
-	// 	return GroupedOpenApi
-	// 		.builder()
-	// 		.group("토큰 필요")
-	// 		.pathsToExclude()
-	// 		.addOpenApiCustomizer(buildSecurityOpenApi())
-	// 		.build();
-	// }
-	//
-	// @Bean
-	// public GroupedOpenApi nonSecurityGroup() {
-	// 	return GroupedOpenApi
-	// 		.builder()
-	// 		.group("토큰 불필요")
-	// 		.pathsToMatch()
-	// 		.build();
-	// }
-	//
-	// private OpenApiCustomizer buildSecurityOpenApi() {
-	// 	SecurityScheme securityScheme = new SecurityScheme()
-	// 		.name(TOKEN_HEADER_NAME)
-	// 		.type(SecurityScheme.Type.HTTP)
-	// 		.in(SecurityScheme.In.HEADER)
-	// 		.bearerFormat("JWT")
-	// 		.scheme("Bearer");
-	//
-	// 	return openApi -> openApi
-	// 		.addSecurityItem(
-	// 			new SecurityRequirement()
-	// 				.addList(TOKEN_HEADER_NAME)
-	// 		)
-	// 		.getComponents()
-	// 		.addSecuritySchemes(TOKEN_HEADER_NAME, securityScheme);
-	// }
-
+    // @Bean
+    // public GroupedOpenApi securityGroup() {
+    // 	return GroupedOpenApi
+    // 		.builder()
+    // 		.group("토큰 필요")
+    // 		.pathsToExclude()
+    // 		.addOpenApiCustomizer(buildSecurityOpenApi())
+    // 		.build();
+    // }
+    //
+    // @Bean
+    // public GroupedOpenApi nonSecurityGroup() {
+    // 	return GroupedOpenApi
+    // 		.builder()
+    // 		.group("토큰 불필요")
+    // 		.pathsToMatch()
+    // 		.build();
+    // }
+    //
+    // private OpenApiCustomizer buildSecurityOpenApi() {
+    // 	SecurityScheme securityScheme = new SecurityScheme()
+    // 		.name(TOKEN_HEADER_NAME)
+    // 		.type(SecurityScheme.Type.HTTP)
+    // 		.in(SecurityScheme.In.HEADER)
+    // 		.bearerFormat("JWT")
+    // 		.scheme("Bearer");
+    //
+    // 	return openApi -> openApi
+    // 		.addSecurityItem(
+    // 			new SecurityRequirement()
+    // 				.addList(TOKEN_HEADER_NAME)
+    // 		)
+    // 		.getComponents()
+    // 		.addSecuritySchemes(TOKEN_HEADER_NAME, securityScheme);
+    // }
 }
