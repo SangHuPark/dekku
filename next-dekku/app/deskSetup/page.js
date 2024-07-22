2c3c7ce37dd0b9d5251f874ffa528942a282ccbd
김민수 <ch05036@naver.com>
2024-07-22 14:52:23 +0900

    feat: add next 기본 세팅 및 페이지 생성

---
+++ next-dekku/app/deskSetup/page.js
@@ -0,0 +1,10 @@
+// 데스크 셋업 페이지
+const DeskSetupPage = () => {
+    return (
+        <div>
+            <h1>데스크셋업</h1>
+        </div>
+    )
+}
+
+export default DeskSetupPage
---
+++ next-dekku/app/page.js
@@ -0,0 +1,10 @@
+// app/page.js
+import MainContent from './components/MainContent';
+
+export default function HomePage() {
+  return (
+    <div>
+      <MainContent />
+    </div>
+  );
+}
