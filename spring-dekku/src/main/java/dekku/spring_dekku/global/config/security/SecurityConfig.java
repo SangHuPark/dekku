641c5856308bbc352a9adccb7ec829dd92be677a
juniorsuk <juniorsuk@naver.com>
2024-07-22 15:05:15 +0900

    feat: set Swagger

--- spring-dekku/src/main/java/dekku/spring_dekku/global/config/security/SecurityConfig.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/global/config/security/SecurityConfig.java
@@ -3,8 +3,13 @@ package dekku.spring_dekku.global.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,4 +25,8 @@ public class SecurityConfig {
                         "/auth/**", "/sign-up/**", "/verification/**",
                         "/users/find-password", "/users/update-password", "/s3/**", "/test");
     }
+
+
 }
+
+
