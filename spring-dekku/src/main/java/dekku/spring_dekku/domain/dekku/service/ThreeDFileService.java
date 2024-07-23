cdfe661622c8af38a0bf4d42c4384b8c9cc234f2
박상후 <tkdgn407@naver.com>
2024-07-23 17:23:13 +0900

    feat: 3D 관련 domain

---
+++ spring-dekku/src/main/java/dekku/spring_dekku/domain/dekku/service/ThreeDFileService.java
@@ -0,0 +1,11 @@
+package dekku.spring_dekku.domain.dekku.service;
+
+import dekku.spring_dekku.domain.dekku.repository.ThreeDFileRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+public interface ThreeDFileService {
+
+
+}
