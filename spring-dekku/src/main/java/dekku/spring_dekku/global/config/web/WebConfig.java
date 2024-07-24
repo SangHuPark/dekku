42b370f4202357beb3f18f2c36b7056adacab482
박상후 <tkdgn407@naver.com>
2024-07-24 10:17:04 +0900

    chore: edit directory name

--- spring-dekku/src/main/java/dekku/spring_dekku/global/config/web/WebConfig.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/global/config/web/WebConfig.java
@@ -1,4 +1,4 @@
-package dekku.spring_dekku.global.config.Web;
+package dekku.spring_dekku.global.config.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
