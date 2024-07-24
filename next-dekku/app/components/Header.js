1fca37e7cdb8d1382e9c681139d5e2634147e09f
권주안 <wndks6450@gmail.com>
2024-07-24 18:04:23 +0900

    style: change Header style

--- next-dekku/app/components/Header.js
+++ next-dekku/app/components/Header.js
@@ -1,35 +1,53 @@
 "use client";
 
 import Link from "next/link";
-import "../styles/Header.css"; // 스타일 파일을 추가로 임포트
+import "../styles/Header.css";
 import { useState } from "react";
 import LoginModal from "../components/LoginModal";
 
 const Header = () => {
   const [showModal, setShowModal] = useState(false);
   return (
-    <header className="header">
-      <div className="logo">
-        <Link href="/">dekku</Link>
+    <header className="flex justify-between p-2 bg-[#333]">
+      <div className="flex items-center">
+        <Link href="/">
+          <img src="/logo.png"/>
+        </Link>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <div>
-              <button className="max-w-4xl mx-auto text-white">
-                <div onClick={() => setShowModal(true)}>로그인</div>
-                <LoginModal showModal={showModal} setShowModal={setShowModal} />
-              </button>
-            </div>
-          </li>
-          <li>
-            <Link href="/threeD">3D Desk</Link>
-          </li>
-          <li>
-            <Link href="/deskSetup">데스크셋업</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="flex flex-col">
+        <div className="flex justify-end text-xs">
+          <nav>
+            <ul>
+              <li className="text-white">고객센터</li>
+              <li className="text-white">마이페이지</li>
+              <li>
+                <div>
+                  <button className="max-w-4xl mx-auto text-white">
+                    <div onClick={() => setShowModal(true)}>로그인</div>
+                    <LoginModal
+                      showModal={showModal}
+                      setShowModal={setShowModal}
+                    />
+                  </button>
+                </div>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="flex font-bold justify-end mt-4 mb-4">
+          <nav>
+            <ul>
+              <li></li>
+              <li>
+                <Link href="/threeD" className="text-xl">3D Desk</Link>
+              </li>
+              <li>
+                <Link href="/deskSetup" className="text-xl">데스크셋업</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 };
