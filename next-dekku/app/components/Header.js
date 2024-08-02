2087c359f80e157066221663943e89fc221609e4
권주안 <wndks6450@gmail.com>
2024-08-02 12:33:51 +0900

    style: 헤더 변경 완료 (커뮤니티 페이지는 아직 없음)

--- next-dekku/app/components/Header.js
+++ next-dekku/app/components/Header.js
@@ -8,49 +8,36 @@ const Header = () => {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    // bg-[#333] text-white
-    <header className="fixed top-0 w-full z-50"> 
-      <div className="max-w-6xl mx-auto flex justify-between items-center p-2">
+    <header className="fixed top-0 w-full z-50 bg-white shadow-lg">
+      <div className="max-w-6xl mx-auto flex justify-between items-center py-6">
         <div className="flex items-center">
           <Link href="/">
             <img src="/logo.png" className="w-20" alt="Logo" />
           </Link>
         </div>
-        <div className="flex flex-col">
-          <div className="flex justify-end text-xs mb-2">
-            <nav>
-              <ul className="flex space-x-4">
-                <li>고객센터</li>
-                <li>마이페이지</li>
-                <li>
-                  <button
-                    onClick={() => setShowModal(true)}
-                  >
-                    로그인
-                  </button>
-                  <LoginModal
-                    showModal={showModal}
-                    setShowModal={setShowModal}
-                  />
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className="flex justify-end text-xl font-bold">
-            <nav>
-              <ul className="flex space-x-6">
-                <li>
-                  <Link href="/threeD">3D 데꾸</Link>
-                </li>
-                <li>
-                  <Link href="/deskSetup">데스크셋업</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
+
+        <div className="flex justify-end">
+          <nav>
+            <ul className="flex space-x-8 text-lg">
+              <li>
+                <Link href="/threeD">3D 데꾸</Link>
+              </li>
+              <li>
+                <Link href="/deskSetup">데스크셋업</Link>
+              </li>
+              <li>
+                <Link href="/">커뮤니티</Link>
+              </li>
+              <li>
+                <button onClick={() => setShowModal(true)}>
+                  <img src="/profile.png" />
+                </button>
+                <LoginModal showModal={showModal} setShowModal={setShowModal} />
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
-      {/* <hr className="border-t-2 border-gray-300" />  */}
     </header>
   );
 };
