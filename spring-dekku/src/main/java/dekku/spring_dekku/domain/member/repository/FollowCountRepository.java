2389e1aa8e83291fe5d2db8995cb282822451f24
박상후 <tkdgn407@naver.com>
2024-08-07 13:10:05 +0900

    #7 0807 dev=>main CI/CD

--- spring-dekku/src/main/java/dekku/spring_dekku/domain/member/repository/FollowCountRepository.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/domain/member/repository/FollowCountRepository.java
@@ -4,7 +4,7 @@ import dekku.spring_dekku.domain.follow.model.entity.Follow;
 import dekku.spring_dekku.domain.member.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FollowRepository extends JpaRepository<Follow, Long> {
+public interface FollowCountRepository extends JpaRepository<Follow, Long> {
     int countByFromMember(Member fromMember);
     int countByToMember(Member toMember);
 }
