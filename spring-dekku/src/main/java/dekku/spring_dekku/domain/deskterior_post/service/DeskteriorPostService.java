1c7293d2414f74c4b1dc5d2e9077d0cd98005f08
박지환 <bat522@naver.com>
2024-08-07 17:13:43 +0900

    feat: deskterior-post update

--- spring-dekku/src/main/java/dekku/spring_dekku/domain/deskterior_post/service/DeskteriorPostService.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/domain/deskterior_post/service/DeskteriorPostService.java
@@ -3,6 +3,7 @@ package dekku.spring_dekku.domain.deskterior_post.service;
 import dekku.spring_dekku.domain.deskterior_post.model.dto.request.CreateDeskteriorPostRequestDto;
 import dekku.spring_dekku.domain.deskterior_post.model.dto.response.CreateDeskteriorPostResponseDto;
 import dekku.spring_dekku.domain.deskterior_post.model.dto.response.FindDeskteriorPostResponseDto;
+import dekku.spring_dekku.domain.deskterior_post.model.dto.response.UpdateDeskteriorPostRequestDto;
 import dekku.spring_dekku.domain.deskterior_post.model.entity.DeskteriorPost;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface DeskteriorPostService {
     List<DeskteriorPost> findAll();
 
     DeskteriorPost findById(Long id);
+
+    DeskteriorPost updateDeskteriorPost(Long id, UpdateDeskteriorPostRequestDto requestDto);
 }
