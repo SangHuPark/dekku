87bdcd755c37fddb07814d0328afdaaa18ee3129
YqqaH <wndks6450@gmail.com>
2024-08-08 23:02:48 +0900

    feat: header style change according to page route

--- next-dekku/app/components/Header.js
+++ next-dekku/app/components/Header.js
@@ -3,15 +3,28 @@
 import Link from "next/link";
 import { useLogin } from "./AuthContext";
 import LoginModal from "./LoginModal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation"; // 수정된 부분
 
 const Header = () => {
   const { isLoggedIn } = useLogin();
   const [showModal, setShowModal] = useState(false);
+  const pathname = usePathname(); // usePathname 훅을 사용하여 pathname을 가져옵니다.
+
+  const [headerClasses, setHeaderClasses] = useState('max-w-6xl mx-auto flex justify-between');
+
+  useEffect(() => {
+    // 경로에 따라 클래스를 설정합니다.
+    if (pathname === '/threeD') {
+      setHeaderClasses('flex justify-between');
+    } else {
+      setHeaderClasses('max-w-6xl mx-auto flex justify-between');
+    }
+  }, [pathname]); // pathname이 변경될 때마다 useEffect가 호출됩니다.
 
   return (
     <header className="fixed top-0 w-full z-50 bg-white px-4 py-6">
-      <div className="max-w-6xl mx-auto flex justify-between">
+      <div className={headerClasses}>
         <div className="flex items-center">
           <Link href="/">
             <img src="/logo.png" className="w-20" alt="Logo" />
