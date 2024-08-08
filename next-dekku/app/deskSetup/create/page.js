e70307a7346a65af6f581842dfbc908dbc424cd0
김민수 <ch05036@naver.com>
2024-08-08 09:53:30 +0900

    update: fe/feature/3dmoving

--- next-dekku/app/deskSetup/create/page.js
+++ next-dekku/app/deskSetup/create/page.js
@@ -1,7 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useRouter } from 'next/router';
+import { useState } from "react";
 
 export default function Next14() {
   const [image, setImage] = useState(null);
@@ -9,7 +8,7 @@ export default function Next14() {
   const [content, setContent] = useState("");
   const [styleInfo, setStyleInfo] = useState("");
   const [colorInfo, setColorInfo] = useState("");
-  const [textureInfo, setTextureInfo] = useState("");
+  const [jobInfo, setJobInfo] = useState("");
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
@@ -19,14 +18,14 @@ export default function Next14() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission
-    console.log({ image, title, content, styleInfo, colorInfo, textureInfo });
+    console.log({ image, title, content, styleInfo, colorInfo, jobInfo });
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="max-w-6xl mx-auto flex justify-center">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col items-center w-3/4 space-y-5"
+        className="flex flex-col w-3/4 space-y-5"
       >
         <div className="flex flex-row w-full space-x-5">
           <div className="flex-1 flex justify-center items-center border border-gray-300 bg-gray-200 p-8">
@@ -96,8 +95,8 @@ export default function Next14() {
             </div>
             <div className="w-full">
               <select
-                value={textureInfo}
-                onChange={(e) => setTextureInfo(e.target.value)}
+                value={jobInfo}
+                onChange={(e) => setJobInfo(e.target.value)}
                 className="w-full p-2 border border-gray-300 rounded"
               >
                 <option value="">직업 정보 추가</option>
@@ -106,10 +105,16 @@ export default function Next14() {
             </div>
           </div>
         </div>
-        <button type="submit" className="bg-black text-white py-2 px-4 rounded">
-          제출
-        </button>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="bg-black text-white py-2 px-4 rounded"
+          >
+            제출
+          </button>
+        </div>
       </form>
     </div>
   );
 }
+
