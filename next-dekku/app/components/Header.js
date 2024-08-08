1c95ed1080633e3c6429b9a250e2b8a4f3247178
권주안 <wndks6450@gmail.com>
2024-08-08 11:07:35 +0900

    feat: add logout

--- next-dekku/app/components/Header.js
+++ next-dekku/app/components/Header.js
@@ -1,9 +1,10 @@
 "use client";
 
 import Link from "next/link";
+import { useLogin } from "./AuthContext";
 
 const Header = () => {
- 
+  const { isLoggedIn } = useLogin();
   return (
     <header className="fixed top-0 w-full z-50 bg-white px-4 py-8">
       <div className="max-w-6xl mx-auto flex justify-between">
@@ -16,14 +17,26 @@ const Header = () => {
         <nav className="flex items-center">
           <ul className="flex items-center space-x-8 text-lg">
             <li>
-              <Link href="/threeD">3D Desk</Link>
+              <Link href="/threeD">3D 데꾸</Link>
             </li>
             <li>
-              <Link href="/deskSetup">Desk Setup</Link>
+              <Link href="/deskSetup">데스크 셋업</Link>
             </li>
-            <li>
-              <Link href="/login">로그인</Link>
-            </li>
+            {isLoggedIn && (
+              <li>
+                <Link href="/profile">마이페이지</Link>
+              </li>
+            )}
+            {!isLoggedIn && (
+              <li>
+                <Link href="/login">로그인</Link>
+              </li>
+            )}
+            {isLoggedIn && (
+              <li>
+                <Link href="/logout">로그아웃</Link>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
