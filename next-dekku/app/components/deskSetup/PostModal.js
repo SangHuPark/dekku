c415722584aa280a9ab2b12c80fc33fa206c3d7c
김민수 <ch05036@naver.com>
2024-08-08 15:33:47 +0900

    feature: add threeDafter page에서 저장 공유 버튼 눌렀을 때 각 동작 구현. 저장, 공유 시 모달 생성

--- next-dekku/app/components/deskSetup/PostModal.js
+++ next-dekku/app/components/deskSetup/PostModal.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const SaveModal = ({ isOpen, onClose }) => {
+const PostModal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg p-6 w-3/4 md:w-1/2 lg:w-1/3">
         <div className="text-center">
-          <p className="text-xl mb-4">성공적으로 저장되었습니다! 저장된 모델은 마이페이지에서 확인 가능합니다! 🎉</p>
+          <p className="text-xl mb-4">게시글이 성공적으로 생성되었습니다! 🎉</p>
           <button 
             onClick={onClose} 
             className="bg-blue-500 text-white py-2 px-4 rounded"
@@ -20,4 +20,4 @@ const SaveModal = ({ isOpen, onClose }) => {
   );
 };
 
-export default SaveModal;
+export default PostModal;
