79540c73ef67ecd1264830817de63686628c01ed
juniorsuk <juniorsuk@naver.com>
2024-08-09 17:12:19 +0900

    feat: nametag

--- next-dekku/app/components/threeD/ProductList.js
+++ next-dekku/app/components/threeD/ProductList.js
@@ -24,7 +24,8 @@ const products = {
   ],
   '기타': [
     { name: '자동차', description: '파란색 자동차', image: '/products_image/파란색자동차.png', modelPath: 'threedmodels/car_example.glb', scale: [0.2, 0.2, 0.2] },
-    { name: '마샬스피커', description: '마샬스피커', image: '/products_image/마샬스피커.png', modelPath: 'threedmodels/speaker_example.glb', scale: [0.04, 0.04, 0.04] }
+    { name: '마샬스피커', description: '마샬스피커', image: '/products_image/마샬스피커.png', modelPath: 'threedmodels/speaker_example.glb', scale: [0.04, 0.04, 0.04] },
+    { name: 'SSAFY 명패', description: '싸피인의 필수품', image: '/products_image/nametag.png', modelPath: 'threedmodels/nametag.glb', scale: [2, 2, 2] },
   ],
 };
 
