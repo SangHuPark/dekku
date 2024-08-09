1c8ad4c47298d545c8aefe59b18ad0f0da93dcdd
김재윤 <joajy98@gmail.com>
2024-08-09 22:57:29 +0900

    feat: add identifier about exists of Like instance

--- spring-dekku/src/main/java/dekku/spring_dekku/domain/like/repository/LikeRepository.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/domain/like/repository/LikeRepository.java
@@ -1,8 +1,11 @@
 package dekku.spring_dekku.domain.like.repository;
 
+import dekku.spring_dekku.domain.deskterior_post.model.entity.DeskteriorPost;
 import dekku.spring_dekku.domain.like.model.entity.Like;
 import dekku.spring_dekku.domain.member.model.entity.Member;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,5 @@ import java.util.List;
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     List<Like> findByMember(Member member);
+    boolean existsByMemberAndDeskteriorPost(Member member, DeskteriorPost post);
 }
