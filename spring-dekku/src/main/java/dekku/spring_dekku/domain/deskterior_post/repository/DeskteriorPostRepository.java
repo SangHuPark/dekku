670d40c1ac258c7b499b578d93ff2087bd89a6d1
김재윤 <joajy98@gmail.com>
2024-08-10 22:40:29 +0900

    feat: view top 3 liked posts repository

--- spring-dekku/src/main/java/dekku/spring_dekku/domain/deskterior_post/repository/DeskteriorPostRepository.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/domain/deskterior_post/repository/DeskteriorPostRepository.java
@@ -1,8 +1,8 @@
 package dekku.spring_dekku.domain.deskterior_post.repository;
 
 import dekku.spring_dekku.domain.deskterior_post.model.entity.DeskteriorPost;
-import dekku.spring_dekku.domain.deskterior_post.model.entity.DeskteriorPostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface DeskteriorPostRepository extends JpaRepository<DeskteriorPost, Long> {
     List<DeskteriorPost> findByMemberId(Long memberId);
+    @Query(value = "SELECT D FROM DeskteriorPost D ORDER BY D.likeCount DESC LIMIT 3")
+    List<DeskteriorPost> findTopPosts();
 }
