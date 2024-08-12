3c47c406f59342b2585104279c476b2ce8ec4cca
SangHuPark <tkdgn5408407@gmail.com>
2024-08-12 20:44:04 +0900

    remove: corsMapping

--- spring-dekku/src/main/java/dekku/spring_dekku/global/config/web/WebConfig.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/global/config/web/WebConfig.java
@@ -10,14 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-			.allowedOrigins("http://localhost:3000", "https://dekku.co.kr")
-			.allowCredentials(true);
-//			.allowedOrigins("*");
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/api/**")
+//			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//			.allowedOrigins("http://localhost:3000", "https://dekku.co.kr")
+//			.allowCredentials(true);
+//	}
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
