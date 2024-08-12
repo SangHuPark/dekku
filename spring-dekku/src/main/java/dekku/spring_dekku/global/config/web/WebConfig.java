4fda5ec59b6b392dd0bce2b8044397bc68c2d8a9
SangHuPark <tkdgn5408407@gmail.com>
2024-08-13 01:22:33 +0900

    feat: addCorsMapping

--- spring-dekku/src/main/java/dekku/spring_dekku/global/config/web/WebConfig.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/global/config/web/WebConfig.java
@@ -10,13 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/api/**")
-//			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//			.allowedOrigins("http://localhost:3000", "https://dekku.co.kr")
-//			.allowCredentials(true);
-//	}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedOrigins("http://localhost:3000")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
