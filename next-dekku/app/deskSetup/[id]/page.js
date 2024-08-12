0c0a54d7c963124d10f3b7ef898ef27e951822ed
박상후 <tkdgn407@naver.com>
2024-08-12 11:11:01 +0900

    #7 0812 11:10 dev=>main CD

--- next-dekku/app/deskSetup/[id]/page.js
+++ next-dekku/app/deskSetup/[id]/page.js
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import ThreeJSRenderer from "../../components/threeD/ThreeJSRenderer"; // ThreeJSRenderer 임포트
+import DeskSetupCard from "../DeskSetupCard";
 
 export default function Details({ params }) {
   const postId = parseInt(params.id, 10); // 문자열을 정수로 변환
