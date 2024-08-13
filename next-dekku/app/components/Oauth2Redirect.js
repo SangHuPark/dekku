b3a8ba66e1e64d096137b3fb9be5521a4267a814
박상후 <tkdgn407@naver.com>
2024-08-13 09:29:03 +0900

    fix: redirect uri

--- next-dekku/app/components/Oauth2Redirect.js
+++ next-dekku/app/components/Oauth2Redirect.js
@@ -11,7 +11,7 @@ const OAuth2Redirect = () => {
   useEffect(() => {
     const OAuth2JwtHeaderFetch = async () => {
       try {
-        const response = await fetch("http://dekku.co.kr:8080/oauth2-jwt-header", {
+        const response = await fetch("http://dekku.co.kr:8080/api/oauth2-jwt-header", {
           method: "POST",
           credentials: "include",
         });
