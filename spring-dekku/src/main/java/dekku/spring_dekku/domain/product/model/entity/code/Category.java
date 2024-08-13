5c5b5ccc69250d3c937c81e6ca24f7932f0e9d3a
박지환 <bat522@naver.com>
2024-08-13 19:22:01 +0900

    src/main/java/dekku/spring_dekku/domain/product

--- spring-dekku/src/main/java/dekku/spring_dekku/domain/product/model/entity/code/Category.java
+++
@@ -1,5 +0,0 @@
-package dekku.spring_dekku.domain.product.model.entity.code;
-
-public enum Category {
-    MONITOR, LAPTOP, MOUSE, KEYBOARD, DESK, ETC
-}
--- spring-dekku/src/main/java/dekku/spring_dekku/domain/product/model/entity/code/ExistStatus.java
+++
@@ -1,5 +0,0 @@
-package dekku.spring_dekku.domain.product.model.entity.code;
-
-public enum ExistStatus {
-    NOT_EXIST, EXIST;
-}
