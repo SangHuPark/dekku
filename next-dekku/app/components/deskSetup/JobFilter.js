52aef521d21ed9885b457184867b9f1598f18a5b
권주안 <wndks6450@gmail.com>
2024-08-15 18:46:15 +0900

    fix: fix filter & sort feat

--- next-dekku/app/components/deskSetup/JobFilter.js
+++ next-dekku/app/components/deskSetup/JobFilter.js
@@ -11,7 +11,7 @@ export default function JobFilter({ jobFilter, setJobFilter }) {
       onChange={(e) => setJobFilter(e.target.value)}
       className="font-pretendard font-bold p-2 bg-[#F5F5F5] text-[#757575] w-30"
     >
-      <option value="NON_SELECT">직업 정보 추가</option>
+      <option value="all">직업 정보 추가</option>
       <option value="OFFICE_WORKER">회사원</option>
       <option value="DEVELOPER">개발자</option>
       <option value="ARCHITECT">건축</option>
