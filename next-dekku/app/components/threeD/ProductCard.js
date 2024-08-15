3dd1dbdbba7d4c64ff4c2311a8b52affc6ddf3a1
김민수 <ch05036@naver.com>
2024-08-15 17:44:39 +0900

    feature: 전체 상품 조회 후 card에 적용. NevBar 적용

--- next-dekku/app/components/threeD/ProductCard.js
+++ next-dekku/app/components/threeD/ProductCard.js
@@ -1,11 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 
 // ProductCard 컴포넌트 정의
-const ProductCard = ({ id, name, description, image, modelPath, scale, price, addProduct, small }) => {
+const ProductCard = ({ id, name, description, imageUrl, modelPath, scale, price, addProduct, small }) => {
   // 클릭 시 호출되는 핸들러 함수
   const handleClick = () => {
     const uniqueId = uuidv4(); // 고유한 uniqueId 생성
-    addProduct && addProduct({ id, uniqueId, name, description, image, modelPath, scale, price  }); // 제품 추가 함수 호출
+    addProduct && addProduct({ id, uniqueId, name, description, imageUrl, modelPath, scale, price  }); // 제품 추가 함수 호출
   };
 
   return (
@@ -16,8 +16,8 @@ const ProductCard = ({ id, name, description, image, modelPath, scale, price, ad
       onClick={handleClick} // 클릭 시 handleClick 함수 호출
     >
       <div className={`flex items-center justify-center mb-1 ${small ? 'w-24 h-24' : 'w-30 h-30'}`}>
-        {image ? (
-          <img src={image} alt={name} className="object-fill w-full h-70rem" />
+        {imageUrl ? (
+          <img src={imageUrl} alt={name} className="object-fill w-full h-70rem" />
         ) : (
           <span className="text-gray-500 text-sm">No Image</span>
         )}
@@ -25,7 +25,7 @@ const ProductCard = ({ id, name, description, image, modelPath, scale, price, ad
       <div className="text-center px-2 py-3">
         <p className="text-xs mb-1 truncate" style={{ fontSize: '12px' }}>{name}</p>
         <p className="text-xs text-gray-500 truncate" style={{ fontSize: '12px' }}>{description}</p>
-        <p className="text-xs text-gray-700" style={{ fontSize: '12px' }}>{price}</p>
+        <p className="text-xs text-gray-700" style={{ fontSize: '12px' }}>{price}원</p>
       </div>
     </div>
   );
