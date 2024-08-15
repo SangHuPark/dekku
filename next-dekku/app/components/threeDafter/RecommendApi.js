df203128a321314ba59a3d6615ade7f64ee644f0
박상후 <tkdgn407@naver.com>
2024-08-15 15:06:41 +0900

    #7 0815 15:05 dev=>main --fix: [fe] uri

--- next-dekku/app/components/threeDafter/RecommendApi.js
+++ next-dekku/app/components/threeDafter/RecommendApi.js
@@ -1,22 +1,28 @@
+// API 호출 함수
 export const fetchRecommendedPosts = async (productIds) => {
-    try {
-      const response = await fetch(`https://dekku.co.kr/api/products/deskterior-posts-by-products`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to fetch recommended posts');
-      }
-  
-      const data = await response.json();
-      console.log(data)
-      return data;
-    } catch (error) {
-      console.error("Error fetching recommended posts:", error);
-      return [];
+  try {
+    // productIds를 쿼리 파라미터로 변환
+    const query = productIds.map(id => `productIds=${id}`).join('&');
+    
+    // API 호출
+    const response = await fetch(`https://dekku.co.kr/api/products/deskterior-posts-by-products?${query}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    // 응답이 성공적이지 않으면 에러 던지기
+    if (!response.ok) {
+      throw new Error('Failed to fetch recommended posts');
     }
-  };
-  
+
+    // JSON 데이터 파싱
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching recommended posts:", error);
+    return [];
+  }
+};
