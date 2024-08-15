65493a753099484f36b2662dd43c776cf3efec01
박상후 <tkdgn407@naver.com>
2024-08-15 16:16:00 +0900

    #7 0815 16:15 dev=>main --feat: [infra] Timezone

--- spring-dekku/src/main/java/dekku/spring_dekku/domain/member/model/dto/response/CreateMyPageResponseDto.java
+++ spring-dekku/src/main/java/dekku/spring_dekku/domain/member/model/dto/response/CreateMyPageResponseDto.java
@@ -3,11 +3,10 @@ package dekku.spring_dekku.domain.member.model.dto.response;
 import java.util.List;
 
 import dekku.spring_dekku.domain.deskterior_post.model.dto.response.FindDeskteriorPostResponseDto;
-import dekku.spring_dekku.domain.deskterior_post.model.entity.DeskteriorPost;
 
 public record CreateMyPageResponseDto (
         String nickname,
-        String image_url,
+        String imageUrl,
         String introduction,
         int followingCount,
         int followerCount,
