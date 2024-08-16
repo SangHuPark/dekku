38bfc3010f5c28c52ec316c1b3219edf379191d1
김민수 <ch05036@naver.com>
2024-08-16 12:51:51 +0900

    error: 불러오기 기능 실패

--- next-dekku/app/components/threeD/ThreeJSRenderer.js
+++ next-dekku/app/components/threeD/ThreeJSRenderer.js
@@ -7,17 +7,27 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import MouseControls from "./MouseControls";
 import TransformControls from "./TransformControls";
 import { v4 as uuidv4 } from "uuid";
-import { useRouter } from "next/navigation";
+
+// 모델 데이터를 fetch하는 함수
+const fetchModelData = async (jsonUrl) => {
+  try {
+    const response = await fetch(jsonUrl);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch model data from ${jsonUrl}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching model data:", error);
+    return null;
+  }
+};
 
 const ThreeJSRenderer = ({
-  selectedProducts,
-  setSelectedProducts,
-  onComplete,
   jsonUrl,
 }) => {
   const mountRef = useRef(null);
   const controlsRef = useRef(null);
-  const router = useRouter();
   const [deskHeight, setDeskHeight] = useState(0);
   const [deskSize, setDeskSize] = useState({ x: 0, z: 0 });
   const [models, setModels] = useState([]);
@@ -26,88 +36,36 @@ const ThreeJSRenderer = ({
   const [renderer, setRenderer] = useState(null);
   const [activeModel, setActiveModel] = useState(null);
   const [deskCenter, setDeskCenter] = useState(new THREE.Vector3());
-  const [relatedPosts, setRelatedPosts] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]); // 상태로 selectedProducts 추가
 
-  // Function to save model data to localStorage
-  const saveModelData = () => {
-    const data = models.map((model) => ({
-      id: model.userData.id,
-      uniqueId: model.userData.uniqueId,
-      name: model.userData.product.name,
-      description: model.userData.product.description,
-      imageUrl: model.userData.product.imageUrl,
-      modelPath: model.userData.product.modelPath,
-      position: model.position.toArray(),
-      scale: model.scale.toArray(),
-      rotation: model.rotation.toArray(),
-      price: model.userData.product.price,
-      isFetched: model.userData.isFetched,
-    }));
-    localStorage.setItem("sceneState", JSON.stringify(data));
-  };
-
-  // Function to capture thumbnail
-  const captureThumbnail = () => {
-    renderer.render(scene, camera);
-    const canvas = renderer.domElement;
-
-    // Create a larger canvas for a better quality thumbnail
-    const thumbnailCanvas = document.createElement('canvas');
-    const ctx = thumbnailCanvas.getContext('2d');
-    thumbnailCanvas.width = 400; // Increase size for better quality
-    thumbnailCanvas.height = (400 / canvas.width) * canvas.height;
-    ctx.drawImage(canvas, 0, 0, thumbnailCanvas.width, thumbnailCanvas.height);
-
-    const thumbnail = thumbnailCanvas.toDataURL('image/jpeg', 0.85); // Increase quality
-    localStorage.setItem("thumbnail", thumbnail);
-    return thumbnail;
-  };
-
-  const loadModelsFromData = (data, scene, loader, setSelectedProducts) => {
+  const loadModelsFromData = (data, scene, loader) => {
     data.forEach((modelData) => {
       loader.load(
         modelData.modelPath,
         (gltf) => {
           const model = gltf.scene;
           if (modelData.position && modelData.scale && modelData.rotation) {
+            // "XYZ"를 제거하고 3개의 rotation 값만 사용
+            const rotation = modelData.rotation.slice(0, 3);
             model.position.fromArray(modelData.position);
             model.scale.fromArray(modelData.scale);
-            model.rotation.fromArray(modelData.rotation);
+            model.rotation.fromArray(rotation); // 수정된 rotation 값 적용
           } else {
-            model.position.set(deskCenter.x, deskHeight, deskCenter.z); // 모델을 책상 중앙에 배치
+            model.position.set(deskCenter.x, deskHeight, deskCenter.z);
             model.scale.set(1, 1, 1);
           }
-
           model.userData = {
             id: modelData.id,
             uniqueId: modelData.uniqueId || uuidv4(),
-            product: {
-              ...modelData,
-              imageUrl: modelData.imageUrl
-            },
+            product: { ...modelData, imageUrl: modelData.imageUrl },
             isFetched: true,
           };
-          console.log(modelData);
-          // 그림자 설정
           model.castShadow = true;
           model.receiveShadow = true;
-
           setModels((prevModels) => [...prevModels, model]);
           setSelectedProducts((prevProducts) => [
             ...prevProducts,
-            {
-              id: modelData.id,
-              name: modelData.name,
-              description: modelData.description,
-              imageUrl: modelData.imageUrl,
-              modelPath: modelData.modelPath,
-              scale: modelData.scale,
-              uniqueId: modelData.uniqueId || uuidv4(),
-              position: modelData.position,
-              rotation: modelData.rotation,
-              price: modelData.price,
-              isFetched: true,
-            },
+            { ...modelData, uniqueId: modelData.uniqueId || uuidv4() },
           ]);
           scene.add(model);
         },
@@ -138,7 +96,7 @@ const ThreeJSRenderer = ({
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth - 543.5, window.innerHeight);
-    renderer.shadowMap.enabled = true; // 그림자 활성화
+    renderer.shadowMap.enabled = true;
     mount.appendChild(renderer.domElement);
     setRenderer(renderer);
 
@@ -146,9 +104,9 @@ const ThreeJSRenderer = ({
     scene.add(ambientLight);
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 3.5);
-    directionalLight.position.set(30, 30, -30); // 왼쪽 대각선에서 오른쪽 대각선으로 빛을 비추도록 위치 조정
-    directionalLight.target.position.set(0, 0, -1.5); // 책상 중심을 향하게 설정
-    directionalLight.castShadow = true; // 그림자 생성 활성화
+    directionalLight.position.set(30, 30, -30);
+    directionalLight.target.position.set(0, 0, -1.5);
+    directionalLight.castShadow = true;
     directionalLight.shadow.mapSize.width = 1024;
     directionalLight.shadow.mapSize.height = 1024;
     directionalLight.shadow.camera.near = 0.5;
@@ -161,36 +119,32 @@ const ThreeJSRenderer = ({
       const desk = gltf.scene;
       desk.position.set(0, 0, -1.5);
       desk.scale.set(3, 3, 3);
-      desk.castShadow = true; // 그림자를 드리움
-      desk.receiveShadow = true; // 그림자를 받음
+      desk.castShadow = true;
+      desk.receiveShadow = true;
 
       const deskBox = new THREE.Box3().setFromObject(desk);
       setDeskHeight(deskBox.max.y);
       setDeskCenter(deskBox.getCenter(new THREE.Vector3()));
 
       scene.add(desk);
-
-      // 카메라 위치를 책상의 중심으로 설정
       camera.position.set(deskCenter.x, deskCenter.y + 15, deskCenter.z - 10);
       camera.lookAt(deskCenter.x, deskCenter.y, deskCenter.z);
 
-      // Load the room model and position it
       loader.load("/threedmodels/ssafyroom.glb", (roomGltf) => {
         const room = roomGltf.scene;
         room.scale.set(3, 3, 3);
         room.position.set(0, 0, -1.5);
-        room.receiveShadow = true; // 방이 그림자를 받도록 설정
+        room.receiveShadow = true;
         scene.add(room);
 
-        // 로그인 후 복원 시 사용될 로컬스토리지의 sceneState 데이터 로드
-        const savedSceneState = localStorage.getItem("sceneState");
-        if (savedSceneState) {
-          const savedData = JSON.parse(savedSceneState);
-          loadModelsFromData(savedData, scene, loader, setSelectedProducts);
-        } else if (jsonUrl) {
+        // jsonUrl에서 모델 데이터 로드
+        if (jsonUrl) {
+          console.log("Loading 3D model with JSON URL:", jsonUrl);
           fetchModelData(jsonUrl).then((data) => {
             if (data) {
-              loadModelsFromData(data, scene, loader, setSelectedProducts);
+              loadModelsFromData(data, scene, loader);
+            } else {
+              console.log("No data loaded from JSON URL.");
             }
           });
         }
@@ -201,12 +155,12 @@ const ThreeJSRenderer = ({
     const planeMaterial = new THREE.ShadowMaterial({ opacity: 0.5 });
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.rotation.x = -Math.PI / 2;
-    plane.position.y = 0; // 바닥의 높이를 맞추기
-    plane.receiveShadow = true; // 바닥이 그림자를 받도록 설정
+    plane.position.y = 0;
+    plane.receiveShadow = true;
     scene.add(plane);
 
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(deskCenter.x, deskCenter.y, deskCenter.z); // OrbitControls의 타겟을 책상의 중심으로 설정
+    controls.target.set(deskCenter.x, deskCenter.y, deskCenter.z);
     controlsRef.current = controls;
 
     const animate = () => {
@@ -217,7 +171,6 @@ const ThreeJSRenderer = ({
 
     animate();
 
-    // 화면 크기 변경 시, 너비를 다시 계산하여 renderer 크기 업데이트
     const handleResize = () => {
       const width = window.innerWidth - 543.5;
       const height = window.innerHeight;
@@ -235,10 +188,9 @@ const ThreeJSRenderer = ({
   }, [jsonUrl]);
 
   useEffect(() => {
-    // 씬 초기화 로직 추가
     const clearScene = () => {
       models.forEach((model) => scene.remove(model));
-      setModels([]); // 기존 모델 상태 초기화
+      setModels([]);
     };
 
     if (scene) {
@@ -257,14 +209,11 @@ const ThreeJSRenderer = ({
               product.modelPath,
               (gltf) => {
                 const model = gltf.scene;
-
-                // scale 값이 배열 형태로 들어온 것을 확인하였으므로, 그대로 적용합니다.
-                model.scale.fromArray(product.scale || [1, 1, 1]); // scale 값 설정
-
+                model.scale.fromArray(product.scale || [1, 1, 1]);
                 if (product.position) {
                   model.position.fromArray(product.position);
                 } else {
-                  model.position.set(deskCenter.x, deskHeight, deskCenter.z); // 기본 위치를 책상 중앙으로 설정
+                  model.position.set(deskCenter.x, deskHeight, deskCenter.z);
                 }
 
                 model.userData = {
@@ -274,7 +223,6 @@ const ThreeJSRenderer = ({
                   isFetched: product.isFetched || false,
                 };
 
-                // 그림자 설정
                 model.castShadow = true;
                 model.receiveShadow = true;
 
@@ -346,53 +294,6 @@ const ThreeJSRenderer = ({
     setActiveModel(null);
   };
 
-  const handleComplete = async () => {
-    saveModelData();
-    const thumbnail = captureThumbnail();
-
-    localStorage.setItem("selectedProducts", JSON.stringify(selectedProducts));
-
-    const sceneState = JSON.parse(localStorage.getItem("sceneState")) || [];
-    const sceneStateIds = sceneState.map((item) => item.id);
-
-    localStorage.setItem("sceneStateIds", JSON.stringify(sceneStateIds));
-
-    console.log('IDs stored in sceneStateIds:', sceneStateIds);
-
-    try {
-      const response = await fetch('https://dekku.co.kr/api/products/related-posts', {
-        method: 'POST', 
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ productIds: sceneStateIds }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json(); // Parse the response as JSON
-      console.log('Related posts:', data);
-
-      setRelatedPosts(data);
-
-      localStorage.setItem("recommendedPosts", JSON.stringify(data));
-
-    } catch (error) {
-      console.error('Error fetching related posts:', error);
-    }
-
-    onComplete();
-  };
-
-  useEffect(() => {
-    const recommendedPosts = JSON.parse(localStorage.getItem("recommendedPosts"));
-    if (recommendedPosts) {
-      console.log('Loaded recommended posts:', recommendedPosts);
-    }
-  }, []);
-
   return (
     <div
       ref={mountRef}
@@ -426,13 +327,6 @@ const ThreeJSRenderer = ({
               onClose={handleCloseTransformControls}
             />
           )}
-          <button
-            className="bg-cyan-800 text-white px-10 py-4 rounded mt-2 fixed bottom-10 right-10 text-lg"
-            onClick={handleComplete}
-            style={{ zIndex: 10 }}
-          >
-            완성하기
-          </button>
         </>
       )}
     </div>
