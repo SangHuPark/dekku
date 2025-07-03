6a322c821a400faf59a61ef8766d434fbd115b13
SangHuPark <tkdgn5408407@gmail.com>
2025-07-03 12:06:19 +0900

    feat: redis lock time constant (#22)

---
+++ backend/dekku/src/main/java/com/a306/dekku/global/constant/Number.java
@@ -0,0 +1,9 @@
+package com.a306.dekku.global.constant;
+
+public class Number {
+
+    // Redisson 락 설정
+    public static final long REDISSON_LOCK_WAIT_TIME_MS = 300;
+    public static final long REDISSON_LOCK_LEASE_TIME_MS = 2000;
+
+}
